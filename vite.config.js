36bdc2f2ea9614fcd9f15a54e19a09ae96701348
Leniza O. Mendes <lenizaomnd@gmail.com>
2026-05-04 21:46:33 -0400

    tgv

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ const HOT_FILE = resolve(__dirname, 'public/hot');
 
 function writeHotFilePlugin() {
     return {
-        name: 'jaiminho-write-hot-file',
+        name: 'doctrinephp-write-hot-file',
         apply: 'serve',
         configureServer(server) {
             server.httpServer?.once('listening', () => {
